ebbdb25e8bc4a7710ea6468581f3ae9736548132
Nikolas <timofeevnik41@gmail.com>
2021-11-19 12:24:26 +0300

    :wrench: Tests for users management

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,21 @@
+import typing as tp
+
 from fastapi.testclient import TestClient
 from app import api
+import random
 
 
 TEST_USER = {"username": "...", "password": "..."}
+FAKE_USER = {"username": "".join(chr(_) for _ in [random.randint(65, 90) for _ in range(10)]), "password": "123456789"}
 
 
 client = TestClient(api)
 
 
-def login() -> str:
+def login(fake: bool = False) -> str:
     """Get jwt token"""
-    token = client.post("/token", data=TEST_USER)
+    if fake:
+        token = client.post("/token", data=FAKE_USER)
+    else:
+        token = client.post("/token", data=TEST_USER)
     return token.json().get("access_token", None)
---
+++ tests/test_users.py
@@ -0,0 +1,43 @@
+import random
+
+from . import client, login, TEST_USER, FAKE_USER
+
+
+def test_get_data_about_unauthorized_user():
+    r = client.get("/api/v1/users/me")
+    assert r.status_code == 401, r.json()
+
+
+def test_get_data_about_authorized_user():
+    token = login()
+    r = client.get("/api/v1/users/me", headers={"Authorization": f"Bearer {token}"})
+    assert r.json().get("user").get("username", None) == TEST_USER.get("username")
+
+
+def test_get_data_concrete_user():
+    token = login()
+    r = client.get(f"/api/v1/users/{TEST_USER.get('username')}", headers={"Authorization": f"Bearer {token}"})
+    assert r.json().get("user").get("username", None) == TEST_USER.get("username")
+    assert isinstance(r.json().get("user").get("is_admin", None), bool)
+
+
+def test_create_new_user():
+    token = login()
+    r = client.post("/api/v1/users/", headers={"Authorization": f"Bearer {token}"}, json=FAKE_USER)
+    assert r.status_code == 200
+
+
+def test_auth_as_a_new_user():
+    token = login(fake=True)
+    r = client.get("/api/v1/users/me", headers={"Authorization": f"Bearer {token}"})
+    assert r.json().get("user").get("username", None), r.json()
+    assert not r.json().get("user").get("is_admin", None), r.json()
+
+
+def test_delete_user():
+    token = login()
+    r = client.delete(f"/api/v1/users/{FAKE_USER.get('username')}", headers={"Authorization": f"Bearer {token}"})
+    assert r.status_code == 200, r.json()
+
+    r = client.get(f"/api/v1/users/{FAKE_USER.get('username')}", headers={"Authorization": f"Bearer {token}"})
+    r.json().get("user", None) is None, r.json()
